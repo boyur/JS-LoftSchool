f3b33e8a5726c9be4218fb15cc1d23a5eb2655d4
Artem Boyur <gboyur@gmail.com>
2016-12-10 12:54:37 +0300

    lesson 3 - dz3 ready

--- lesson3/dz3.js
+++ lesson3/dz3.js
@@ -1,3 +1,42 @@
-function calculator() {
-  
+function calculator(value) {
+  return {
+    sum: function sum() {
+      let result = value;
+      for (let i = 0; i < arguments.length; i++) {
+        result += arguments[i];
+      }
+
+      return result;
+    },
+    dif: function dif() {
+      let result = value;
+      for (let i = 0; i < arguments.length; i++) {
+        result -= arguments[i];
+      }
+
+      return result;
+    },
+    div: function dif() {
+      let result = value;
+      for (let i = 0; i < arguments.length; i++) {
+        result /= arguments[i];
+      }
+
+      return result;
+    },
+    mul: function dif() {
+      let result = value;
+      for (let i = 0; i < arguments.length; i++) {
+        result *= arguments[i];
+      }
+
+      return result;
+    }
+  };
 }
+
+let myCalculator = calculator(100);
+console.log(myCalculator.sum(1, 2, 3)); //вернет 106
+console.log(myCalculator.dif(10, 20)); //вернет 70
+console.log(myCalculator.div(2, 2)); //вернет 25
+console.log(myCalculator.mul(2, 2)); //вернет 400
