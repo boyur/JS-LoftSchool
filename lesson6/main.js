2fc4d706eff5733aaaa64a6874e3e58cc2e6a261
Artem Boyur <gboyur@gmail.com>
2016-12-17 12:10:13 +0300

    lesson6 dz1 final

--- lesson6/main.js
+++ lesson6/main.js
@@ -5,10 +5,16 @@ console.log(accordion);
 accordion.addEventListener('click', function (e) {
   let item = e.target.parentNode;
 
+  let sizeText = item.children[1].textContent.length;
+
+  console.log(item.children[1].textContent.length);
+
   if (item.classList.contains('active')) {
     item.classList.remove('active');
+    item.children[1].style.maxHeight = "0px";
   } else {
     item.classList.add('active');
+    item.children[1].style.maxHeight = Math.round(sizeText / 100) * 18 + "px";
   }
 
 });
